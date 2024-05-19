64ed757e1bf0414a49fe6164af9130a077a00f85
Nassos <74179355+NassimAll@users.noreply.github.com>
2024-05-19 11:01:56 +0200

    Adding dead instruction check

--- TERZOASSIGNMENT/LoopWalk.cpp
+++ TERZOASSIGNMENT/LoopWalk.cpp
@@ -1,3 +1,4 @@
+
 #include "llvm/Transforms/Utils/LoopWalk.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
@@ -42,7 +43,8 @@ bool isInstructionLoopInvariant(Instruction *I, Loop &loop) {
     outs() << *I << " - Istruction is not safe to move \n\n";
     return false;
   }
-  //opInvariant = true;
+
+  // Facciamo il controllo su titti gli operandi dell'istruzione
   for(auto it = I->op_begin(); it != I->op_end(); ++it) {
     if (!isOperandInvariant(*it, loop)) 
       return false;
@@ -52,6 +54,20 @@ bool isInstructionLoopInvariant(Instruction *I, Loop &loop) {
   return true;
 }
 
+bool isDead(Instruction *I, Loop &loop) {
+  bool isDead = true;
+  //Itero sugli usi e se tutti gli usi sono nel for allora l'istruzione si uò reputare dead
+  for(auto u = I->use_begin(); u != I->use_end() && isDead; ++u) {
+    Instruction *instr = dyn_cast<Instruction>(&*u);
+    if(!loop.contains(instr))
+        isDead = false;
+  }
+  outs() << "Instruction " << *I << " isDead: " << isDead << "\n\n";
+  return isDead;
+}
+/*
+  Controllo che le istruzioni siano loop invariant
+*/
 void findInstInvariants(Loop &loop, BasicBlock &block) {
   for(auto &I : block) {
     if (isInstructionLoopInvariant(&I, loop)) {
@@ -60,9 +76,22 @@ void findInstInvariants(Loop &loop, BasicBlock &block) {
       }
   } 
 }
+/*
+  Controllo che le istruzioni siano sia dead che loop invariant
+*/
+void findInstInvariantsDead(Loop &loop, BasicBlock &block) {
+  for(auto &I : block) {
+    if (isInstructionLoopInvariant(&I, loop) && isDead(&I, loop)) {
+        Invariants.insert(&I);
+        InstToMove.push_back(&I);
+      }
+  } 
+}
+
 
 bool runOnLoop(Loop &loop, LoopAnalysisManager &LAM, LoopStandardAnalysisResults &LAR, LPMUpdater &LU) {
 
+  bool analysisDone = false;
    // check if there is a preheader
   BasicBlock* preheader = loop.getLoopPreheader();
   outs() << "--------------------------- PRE-HEADER ----------------------------- \n";
@@ -71,68 +100,70 @@ bool runOnLoop(Loop &loop, LoopAnalysisManager &LAM, LoopStandardAnalysisResults
   if (!preheader) {
     return false;
   }
-  // Creiamo un vettore che contiene tutte le possibili uscite del loop
+  // Carico nel vettore l'uscita/le uscite del nostro loop 
   SmallVector<BasicBlock*> vec {};
   loop.getExitBlocks(vec);
  
-  BasicBlock * exitBlock = loop.getUniqueExitBlock();
   outs() << "--------------------------- EXIT BLOCK ----------------------------- \n";
-  exitBlock->print(outs());
+  for(auto it = vec.begin(); it != vec.end(); ++it) {
+    BasicBlock *exitBlock = *it;
+    exitBlock->print(outs());
+    outs() << "\n\n";
+  }
   outs() << "---------------------------  ----------------------------- \n\n";
 
-
+  //Ottengo il dominator tree e lo stampo
   llvm::DominatorTree &DT = LAR.DT;
   outs() << "--------------------------- DOMINATOR TREE ----------------------------- \n";
   DT.print(outs());
-  outs() << "---------------------------  ----------------------------- \n\n";
+  outs() << "\n\n";
 
-
+  //Semplice stampa del blocco root, non serve per la licm
   BasicBlock *BB = (DT.getRootNode())->getBlock();
   outs() << "--------------------------- ROOT BLOCK ----------------------------- \n";
   BB->print(outs());
   outs() << "---------------------------  ----------------------------- \n\n";
 
-  /*
-    Prima di tutto si controlla se il blocco del loop in esame domina tutte le uscite
-    del loop
-  */
+  //Iteriamo sui blocchi del loop per controllarli tutti singolarmente
   auto loopBlocks = loop.getBlocks();
   for (auto &block : loopBlocks) {
       bool dominateExits = true;
       outs() << "--------------------------- BLOCK ----------------------------- \n";
       block->print(outs());
-      outs() << "---------------------------  ----------------------------- \n\n";
-
-      // dominateExit = DT.dominates(block, exitBlock);
-      // outs() << block->getName() << " - Domina le uscite: " << dominateExit << "\n";
-
+      outs() << "\n\n";
+      /*
+        Prima di tutto si controlla se il blocco del loop in esame domina tutte le uscite
+      */
       for(auto it = vec.begin(); it != vec.end(); ++it) {
           BasicBlock *exitBlock = *it;
-          outs() << "BB Exit: " << exitBlock->getName() << "\n\n";
           if(!DT.dominates(block, exitBlock))
               dominateExits = false;
       }
       
       outs() << block->getName() << " - Domina le uscite: " << dominateExits << "\n";
       /*
-        se il blocco domina tutte le uscite controlliamo se al suo interno sono 
-        presenti delle istruzioni loop invariant
+        Se il blocco domina tutte le uscite cerchiamo al suo interlo le istruzioni loop invariant
+        invece se il blocco non domina tutte le uscite controlliamo che le istruzioni siano 
+        loop invariant e siano morte alla fine del ciclo (ovvero non abbiano usi )
       */
       if (dominateExits) 
         findInstInvariants(loop, *block);
+      else
+        findInstInvariantsDead(loop, *block);
   }
-  //Spostiamo  le istruzioni loop invariant
+
+  // Iteriamo sul vettore che contiene le istruzioni da spostare e le spostiamo
   for (auto &I : InstToMove) {
     outs () << "Inst to move: " << *I << "\n";
     I->moveBefore(preheader->getTerminator());
+    analysisDone = true;
   }
 
   outs() << "--------------------------- NEW PRE-HEADER ----------------------------- \n";
   preheader->print(outs());
   outs() << "---------------------------   ----------------------------- \n\n";
 
-
-  return true;
+  return analysisDone;
 }
 
 
